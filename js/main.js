88f832f3e7070e5f5dff5361c7e875ac8ce6702e
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-09 21:31:43 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let casualNumbers = [];
-
+let userArray = [];
+let guessedNumb= 0;
 
 function getRndNumber(){
     return Math.floor((Math.random() * 100) + 1);
@@ -16,6 +17,32 @@ function fiveNumberGenerator(){
 function printNumbers(){
     casualNumbers = fiveNumberGenerator();
     document.getElementById("message").innerHTML = "Sequenza di numeri " + casualNumbers;
+};
+
+function getUserInput(){
+    let userInput =  document.getElementById("userInput");
+    return parseInt(userInput.value);
+};
+
+function pushIntoAnArray(){
+    if (userArray.length < 5){
+        userArray.push(getUserInput());
+        console.log(userArray.length);
+    } 
+    else {
+        document.getElementById("error").innerHTML = "Non puoi inserire più numeri";
+    }
+}
+
+function checkWin(){
+        for (let i = 0; i < userArray.length; i++) {
+            if (userArray[i] == casualNumbers[i]){
+                document.getElementById("whichNumb").innerHTML = userArray[i];
+                guessedNumb += 1;
+                document.getElementById("guessedNumb").innerHTML = "Il tuo punteggio è " + guessedNumb;
+            }
+            
+        }
 }
 
 document.getElementById("generateButton").addEventListener("click", function(){
@@ -30,6 +57,12 @@ document.getElementById("generateButton").addEventListener("click", function(){
     setTimeout(function(){
         document.getElementById("userInput").style.display="block";
     }, 3001);
+});
 
+document.getElementById("sendInputButton").addEventListener("click", function(){
+    pushIntoAnArray();
+});
 
+document.getElementById("playButton").addEventListener("click", function(){
+    checkWin();
 });
