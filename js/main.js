c67505513e04eb89ef686c55ca1b5be8408013ce
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-09 18:13:00 +0100

    input text appear

--- js/main.js
+++ js/main.js
@@ -13,10 +13,23 @@ function fiveNumberGenerator(){
     return fiveNumbArray;
 };
 
+function printNumbers(){
+    casualNumbers = fiveNumberGenerator();
+    document.getElementById("message").innerHTML = "Sequenza di numeri " + casualNumbers;
+}
+
 document.getElementById("generateButton").addEventListener("click", function(){
 
-    casualNumbers = fiveNumberGenerator();
-    console.log(casualNumbers);
+    //print five numbers and after 30s they disappear
+    printNumbers();
+    setTimeout(function(){
+        document.getElementById("message").innerHTML = "";
+    }, 3000);
+
+    //input type text appear
+    setTimeout(function(){
+        document.getElementById("userInput").style.display="block";
+    }, 3001);
 
 
 });
